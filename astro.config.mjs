0e14904d880a91468aa094ddfd152eda6d08f554
Andrew Davis <atd285@gmail.com>
2023-06-22 16:39:13 -0400

    more experimenting with astro apis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import alpine from '@astrojs/alpinejs';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [alpine()],
+  integrations: [alpine(), tailwind()]
 });
